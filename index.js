62ac2e12ee05db1222e86a1358b1f5c4efb811bf
Subasree2717 <158578935+Subasree2717@users.noreply.github.com>
2025-02-03 12:53:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 app.listen('5000', ()=> console.log('server running on 5000'))
 
-mongoose.connect('mongodb://localhost:27017/sample')
+mongoose.connect('mongodb+srv://admin:suba2717@cluster0.afk34.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(()=> console.log('database connected..'))
     .catch((err) => console.log(err))
 
